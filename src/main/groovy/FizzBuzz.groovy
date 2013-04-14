dd2d7291fa3d864f78d9f317f52c82a16bbaea03
sue445 <sue445@sue445.net>
2013-04-14 15:55:45 +0900

    impl 5n

--- src/main/groovy/FizzBuzz.groovy
+++ src/main/groovy/FizzBuzz.groovy
@@ -7,6 +7,10 @@
  */
 class FizzBuzz {
     String result(int num){
-        "Fizz"
+        if(num % 3 == 0){
+            "Fizz"
+        } else if(num % 5 == 0){
+            "Buzz"
+        }
     }
 }
